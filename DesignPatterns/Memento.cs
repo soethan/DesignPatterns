58416919b6d93489fc35d5ac2603968dab0b07f8
Soe Than <soethan@gmail.com>
2017-07-02 20:48:16 +0800

    Memento.cs

---
+++ DesignPatterns/Memento.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// The 'Originator' class
+    /// </summary>
+    public class SalesProspect
+    {
+        private string _name;
+        private string _phone;
+        private double _budget;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                _phone = value;
+            }
+        }
+
+        public double Budget
+        {
+            get { return _budget; }
+            set
+            {
+                _budget = value;
+            }
+        }
+
+        //Stores memento
+        public Memento SaveMemento()
+        {
+            Console.WriteLine("\nSaving state --\n");
+            return new Memento(_name, _phone, _budget);
+        }
+
+        // Restores memento
+        public void RestoreMemento(Memento memento)
+        {
+            Console.WriteLine("\nRestoring state --\n");
+            this.Name = memento.Name;
+            this.Phone = memento.Phone;
+            this.Budget = memento.Budget;
+        }
+    }
+
+    /// <summary>
+    /// The 'Memento' class
+    /// </summary>
+    public class Memento
+    {
+        private string _name;
+        private string _phone;
+        private double _budget;
+
+        public Memento(string name, string phone, double budget)
+        {
+            this._name = name;
+            this._phone = phone;
+            this._budget = budget;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public double Budget
+        {
+            get { return _budget; }
+            set { _budget = value; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Caretaker' class
+    /// </summary>
+    public class ProspectMemory
+    {
+        private Memento _memento;
+
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -472,6 +472,31 @@ namespace DesignPatterns
 
             #endregion
 
+            #region Memento
+
+            ConsoleWriteWithColor("*********** Memento Pattern starts ***********");
+
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
+
+            // Store internal state
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+
+            // Continue changing originator
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+
+            // Restore saved state
+            s.RestoreMemento(m.Memento);
+
+            ConsoleWriteWithColor("*********** Memento Pattern ends ***********");
+
+            #endregion
+
             #region Observer
 
             ConsoleWriteWithColor("*********** Observer Pattern starts ***********");
