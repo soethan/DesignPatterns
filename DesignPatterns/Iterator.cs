4f497f4bad44d275a3288ace511460285887221b
Soe Than <soethan@gmail.com>
2017-07-02 21:49:04 +0800

    Iterator.cs

---
+++ DesignPatterns/Iterator.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// A collection item
+    /// </summary>
+    public class Item
+    {
+        private string _name;
+
+        // Constructor
+        public Item(string name)
+        {
+            this._name = name;
+        }
+
+        // Gets name
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Aggregate' interface
+    /// </summary>
+    interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+
+    /// <summary>
+    /// The 'ConcreteAggregate' class
+    /// </summary>
+    public class Collection : IAbstractCollection
+    {
+        private ArrayList _items = new ArrayList();
+
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+
+        // Gets item count
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        // Indexer
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Add(value); }
+        }
+    }
+
+    /// <summary>
+    /// The 'Iterator' interface
+    /// </summary>
+    public interface IAbstractIterator
+    {
+        Item First();
+        Item Next();
+        bool IsDone { get; }
+        Item CurrentItem { get; }
+    }
+
+    /// <summary>
+    /// The 'ConcreteIterator' class
+    /// </summary>
+    public class Iterator : IAbstractIterator
+    {
+        private Collection _collection;
+        private int _current = 0;
+        private int _step = 1;
+
+        public Iterator(Collection collection)
+        {
+            this._collection = collection;
+        }
+
+        public Item First()
+        {
+            _current = 0;
+            return _collection[_current] as Item;
+        }
+
+        public Item Next()
+        {
+            _current += _step;
+            if (!IsDone)
+                return _collection[_current] as Item;
+            else
+                return null;
+        }
+
+        public int Step
+        {
+            get { return _step; }
+            set { _step = value; }
+        }
+
+        public Item CurrentItem
+        {
+            get { return _collection[_current] as Item; }
+        }
+
+        // Gets whether iteration is complete
+        public bool IsDone
+        {
+            get { return _current >= _collection.Count; }
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -441,6 +441,43 @@ namespace DesignPatterns
 
             #endregion
 
+            #region Iterator
+
+            ConsoleWriteWithColor("*********** Iterator Pattern starts ***********");
+
+            // Build a collection
+            Collection collection = new Collection();
+            collection[0] = new Item("Item 0");
+            collection[1] = new Item("Item 1");
+            collection[2] = new Item("Item 2");
+            collection[3] = new Item("Item 3");
+            collection[4] = new Item("Item 4");
+            collection[5] = new Item("Item 5");
+            collection[6] = new Item("Item 6");
+            collection[7] = new Item("Item 7");
+            collection[8] = new Item("Item 8");
+
+            Console.WriteLine("collection[3]=" + (collection[3] as Item).Name);
+
+            // Create iterator
+            Iterator iterator = collection.CreateIterator();
+
+            // Skip every other item
+            iterator.Step = 2;
+
+            Console.WriteLine("Iterating over collection:");
+
+            for (Item item = iterator.First();
+                !iterator.IsDone; 
+                item = iterator.Next())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            ConsoleWriteWithColor("*********** Iterator Pattern ends ***********");
+
+            #endregion
+
             #region Mediator
 
             ConsoleWriteWithColor("*********** Mediator Pattern starts ***********");
