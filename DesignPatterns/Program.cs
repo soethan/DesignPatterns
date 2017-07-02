9618ef2aaa26a9733ecb14302f391b96e24fef9a
Soe Than <soethan@gmail.com>
2017-07-02 17:28:28 +0800

    ProxyPattern.cs

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -492,6 +492,23 @@ namespace DesignPatterns
 
             #endregion
 
+            #region Proxy
+
+            ConsoleWriteWithColor("*********** Proxy Pattern starts ***********", ConsoleColor.Yellow);
+
+            // Create math proxy
+            var proxy = new MathProxy();
+
+            // Do the math
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+
+            ConsoleWriteWithColor("*********** Proxy Pattern ends ***********", ConsoleColor.Yellow);
+
+            #endregion
+
             #endregion
 
             #region Service Locator Pattern
